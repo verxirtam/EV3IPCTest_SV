7018a30015090fde053f285aa1e0507749598e49
daisuke <daisuke@daisuke-ubuntu>
2015-07-05 00:38:53 +0900

    initial commit

---
+++ EV3IPCTest_SV/src/EV3IPCTest_SV.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.net.*;
+
+public class EV3IPCTest_SV
+{
+	public static void main(String[] args)
+	{
+		int port = 50000;
+
+		ServerSocket s_s=null;
+		try {
+			s_s = new ServerSocket(port);
+			System.out.println("Ready to Transfer");
+			for (;;) {
+				Socket s = s_s.accept( );
+				new Connect(s).start();
+			}
+		}
+		catch (Exception e)
+		{
+			System.out.println(e);
+		}
+		finally
+		{
+			try
+			{
+				s_s.close();
+			}
+			catch(IOException ioe){}
+		}
+	}
+}
+/*
+          新しい接続
+*/
+class Connect extends Thread
+{
+	private Socket s;
+
+	public Connect(Socket s_i)
+	{
+		s = s_i;
+	}
+
+	public void run()
+	{
+
+		try {
+					// ファイル名
+			PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+			out.println("This is test message by Java.AAAA:0");
+			out.println("This is test message by Java.AAAAAAAAA:1");
+			out.println("This is test message by Java.:2");
+			
+
+			s.close();
+		}
+
+		catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
